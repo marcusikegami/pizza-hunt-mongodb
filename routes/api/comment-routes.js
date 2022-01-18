9c3784b617f4d46a27ea9e8e59c6ab51e91650cc
Marcus Ikegami <hikegami.6@gmail.com>
2022-01-18 15:11:04 -0700

    comment model

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const { addComment, removeComment } = require('../../controllers/comment-controller');
 
+// /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
-router.route('/:pizzaId/:commendId').delete(removeComment);
+// /api/comments/<pizzaId>/<commentId>
+router.route('/:pizzaId/:commentId').delete(removeComment);
 
 module.exports = router;
